ca19dd43f3c50fb1f2273d0cee245c872f9c1b76
enrico76 <enricodip@gmail.com>
2019-09-18 20:04:51 +0200

    add handlebar and generated crud call to database

---
+++ pagamenti.php
@@ -0,0 +1,37 @@
+<?php
+header('Content-type: application/json');
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'design';
+
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+if ($conn -> connect_error) {
+  var_dump('error');
+  var_dump($conn);
+  die();
+}
+
+$query = '
+SELECT *
+FROM pagamenti
+WHERE id >= 100
+';
+
+$res = $conn -> query($query);
+
+$pagamenti = [];
+
+if ($res && $res -> num_rows > 0) {
+  while ($row = $res -> fetch_assoc()) {
+    $pagamenti[] = $row;
+  }
+}
+$conn -> close();
+
+echo json_encode($pagamenti);
+
+
+ ?>
